8c3b772064cec73c8368374f5dae252597e18890
A Vidmar <a.v.vidmar@gmail.com>
2020-02-11 22:23:21 +0100

    new offers added

--- js/script.js
+++ js/script.js
@@ -1,30 +1,47 @@
 var h = window.innerHeight;
 var w = window.innerWidth;
 
+function acceptCookies() {
+    cookies.classList.add("accepted");
+    cookiesSpaceholder.style.height = 0;
+    setCookie("cookies", "accepted", 7)
+}
 
+function clearCookies() {
+    alert("Izbrisano.");
+}
+
+function setCookie(cookieName, cookieValue, expiryDays) {
+    var date = new Date();
+    date.setTime(date.getTime() + (expiryDays*24*60*60*1000));
+    var expires = "expires="+ date.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+var cookiesAccepted = getCookie('cookies');
 var cookies = document.getElementById('cookies');
 var cookies_h = cookies.offsetHeight;
-console.log(cookies, cookies_h);
 var cookiesSpaceholder = document.getElementById('cookies-spaceholder');
+var cookiesNoticeShown = true;
 
-// var cookies_left = document.querySelector('.cookies__left');
-// var cookies_right = document.querySelector('.cookies__right');
-// var cookies_right_h = cookies_right.offsetWidth;
-// cookies_left.style.paddingRight = cookies_right_h + 'px';
-
-cookiesSpaceholder.style.height = cookies_h + 'px';
-
-
-// var docLang = document.documentElement.lang;
 var header = document.getElementById('header');
 var header_h = 0;
-// var headerHero = document.querySelector('.header__hero');
-// var headerGreen_h = headerGreen.offsetHeight;
-
-var p = document.getElementsByTagName('p');
-var p1_h = p[0].offsetHeight;
-
-console.log(p[0], p1_h);
 
 var video = document.querySelector('.video');
 var video_w = video.offsetWidth;
@@ -32,34 +49,46 @@ var iframe = document.getElementById('video');
 var iframe_h = video_w * 0.5625;
 var iframe_w = video_w;
 
-console.log('hello world');
-console.log('screen height = ' + h, 'screen width = ' + w);
-console.log(video);
-console.log(video_w);
-console.log(iframe);
-console.log(iframe.height);
-
-// <!-- width="560" (x 0.5625) height="315" -->
+// console.log('hello world');
+// console.log('screen height = ' + h, 'screen width = ' + w);
 
 if (video_w > 895) {
     iframe_h = 504;
     iframe_w = 896;
 }
 
+if (cookiesAccepted === 'accepted') {
+    cookies.classList.add("accepted");
+    cookiesSpaceholder.style.height = 0;
+    cookiesNoticeShown = false;
+} else {
+    cookiesSpaceholder.style.height = cookies_h + 'px';
+}
+
 iframe.width = iframe_w;
 iframe.height = iframe_h;
 
-
-// header_h = h - p1_h;
 header_h = h * 0.95;
-// headerHero.style.height = headerHero_h + 'px';
 header.style.height = h + 'px';
 
-function acceptCookies() {
-    cookies.classList.toggle("accepted");
-    cookiesSpaceholder.style.height = 0;
-}
+window.addEventListener('resize', function() {
+    h = window.innerHeight;
+    w = window.innerWidth;
+    video_w = video.offsetWidth;
 
-function clearCookies() {
-    alert("Izbrisano.");
-}
+    if (cookiesNoticeShown) {
+        cookies_h = cookies.offsetHeight;
+    }
+    
+    if (video_w > 895) {
+        iframe_h = 504;
+        iframe_w = 896;
+    } else {
+        iframe_h = video_w * 0.5625;
+        iframe_w = video_w;
+    }
+
+    iframe.width = iframe_w;
+    iframe.height = iframe_h;
+    header.style.height = h + 'px';
+})
